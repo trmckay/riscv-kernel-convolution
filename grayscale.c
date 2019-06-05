88080733d0d7c7317c2e857047c57110ac21b6a6
Trevor McKay <trs.mckay@gmail.com>
2019-06-04 18:34:54 -0700

    rewrote grayscale function in C

---
+++ grayscale.c
@@ -0,0 +1,25 @@
+/* all pixel values in memory <- grayscale equivalent
+   used registers: a0, a1, a2 (color values, not saved),
+                   t0 (current address, not saved), t1 (RGB value, not saved),
+                   t2 (bit-shifted intensity, not saved)
+arguments:
+a0: start address of image
+a1: end of image
+*/
+void grayscale(unsigned char *image, unsigned char *end)
+{
+    // for each pixel
+    for (int i = 0; i < (end-image); i++)
+    {
+        // isolate R, G, and B values
+        unsigned char RGB = image[i];
+        unsigned char red = (RGB & 0b11100000) >> 5;
+        unsigned char green = (RGB & 0b00011100) >> 2;
+        unsigned char blue = (RGB & 0b00000011);
+        unsigned char intensity = (red+green+blue+blue)/3;
+        image[i] = 0;
+        image[i] += intensity << 5;
+        image[i] += intensity << 2;
+        image[i] += intensity >> 1;
+    }
+}
